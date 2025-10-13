43c32cf5f334c55f1ba26bb7f724a13576eeaebf
Muhammad Raffi <muh.raffi2004@gmail.com>
2025-10-13 13:58:07 +0700

    Tambah halaman pelanggan dan erd

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,15 +5,30 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Dashboard Pelanggan</title>
+    <link rel="stylesheet" href="{{ asset('css/style1.css') }}">
 </head>
 <body>
+    <header>
+        <div class="logo">
+            <h3><a class="logoL" href="/">{{ Auth::user()->name }}</a></h3>
+        </div>
+        <nav>
+            <ul id="menu">
+               <li>
+                     <form action="{{ route('logout') }}" method="POST" style="display:inline">
+                        @csrf
+                        <button type="submit">
+                            Logout
+                        </button>
+                    </form>
+                </li>
+            </ul>
+        </nav>
+    </header>
+    <section>
+        
+    </section>
     
-    <form action="{{ route('logout') }}" method="POST" style="display:inline">
-        @csrf
-        <button type="submit">
-            Logout
-        </button>
-    </form>
     
 </body>
 </html>
