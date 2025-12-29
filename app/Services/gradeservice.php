1167720979d53ef652dbb542a85cb5678cb98e54
Raffi <muh.raffi2004@gmail.com>
2025-12-29 13:53:35 +0700

    ubah dikit

--- app/Services/gradeservice.php
+++ app/Services/gradeservice.php
@@ -4,17 +4,20 @@ namespace App\Services;
 
 class GradeService
 {
-    public function calculateGrade(int $score)
+    public function calculateGrade(int $score): string
     {
         if ($score < 0 || $score > 100) {
             return 'INVALID';
         }
+
         if ($score >= 85) {
             return 'A';
         }
+
         if ($score >= 70) {
             return 'B';
         }
+
         return 'C';
     }
 }
